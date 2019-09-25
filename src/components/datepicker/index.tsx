1b16c1ced690ae7171dcc65f49ac53cdedee1452
Veblin <veblin.w@gmail.com>
2019-09-25 20:20:12 +0800

    初步提交

---
+++ src/components/datepicker/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DatePicker from 'antd/es/date-picker';
+
+class ComDatePicker extends React.Component{
+    render () {
+        return (
+            <DatePicker />
+        )
+    }
+}
+
+
+export default ComDatePicker;
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DatePicker from 'antd/es/date-picker';
+
+class Home extends React.Component{
+    render () {
+        return (
+            <DatePicker />
+        )
+    }
+}
+
+
+export default Home;
